cdba657fe7be631f1fac0ff718f78d171fbf9b7c
ishidad2 <ishida_d@opening-line.co.jp>
2024-08-03 17:16:24 +0900

    広告を別ファイルに分割

---
+++ lib/ad_banner.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+class AdBanner extends StatefulWidget {
+  const AdBanner({super.key});
+
+  @override
+  AdBannerState createState() => AdBannerState();
+}
+
+class AdBannerState extends State<AdBanner> {
+  BannerAd? _bannerAd;
+  bool _isAdLoaded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadBannerAd();
+  }
+
+  void _loadBannerAd() {
+    _bannerAd = BannerAd(
+      adUnitId: 'ca-app-pub-3940256099942544/6300978111', // テスト用ID
+      // adUnitId: 'ca-app-pub-8243405002982782/6937685383', // 本番用
+      size: AdSize.banner,
+      request: const AdRequest(),
+      listener: BannerAdListener(
+        onAdLoaded: (ad) {
+          setState(() {
+            _isAdLoaded = true;
+          });
+        },
+        onAdFailedToLoad: (ad, error) {
+          ad.dispose();
+          print('Ad failed to load: $error');
+        },
+      ),
+    );
+
+    _bannerAd!.load();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_isAdLoaded) {
+      return SizedBox(
+        width: _bannerAd!.size.width.toDouble(),
+        height: _bannerAd!.size.height.toDouble(),
+        child: AdWidget(ad: _bannerAd!),
+      );
+    } else {
+      return const SizedBox.shrink();
+    }
+  }
+
+  @override
+  void dispose() {
+    _bannerAd?.dispose();
+    super.dispose();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'ad_banner.dart'; // ad_banner.dartファイルをインポート
 
 void main() {
   // Mobile Ads SDKの初期化
   WidgetsFlutterBinding.ensureInitialized();
   MobileAds.instance.initialize();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,70 +29,11 @@ class MyApp extends StatelessWidget {
             // 底部にバナー広告を固定
             Align(
               alignment: Alignment.bottomCenter,
-              child: AdBanner(),
+              child: AdBanner(),  // AdBannerを表示
             ),
           ],
         ),
       ),
     );
-  }
-}
-
-class AdBanner extends StatefulWidget {
-  const AdBanner({super.key});
-
-  @override
-  AdBannerState createState() => AdBannerState();
-}
-
-class AdBannerState extends State<AdBanner> {
-  BannerAd? _bannerAd;
-  bool _isAdLoaded = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _loadBannerAd();
-  }
-
-  void _loadBannerAd() {
-    _bannerAd = BannerAd(
-      adUnitId: 'ca-app-pub-3940256099942544/6300978111', // テスト用IDを使用。実際には自身のAd Unit IDを使用
-      // adUnitId: 'ca-app-pub-8243405002982782/6937685383', //本番用
-      size: AdSize.banner,
-      request: const AdRequest(),
-      listener: BannerAdListener(
-        onAdLoaded: (ad) {
-          setState(() {
-            _isAdLoaded = true;
-          });
-        },
-        onAdFailedToLoad: (ad, error) {
-          ad.dispose();
-          print('Ad failed to load: $error');
-        },
-      ),
-    );
-
-    _bannerAd!.load();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (_isAdLoaded) {
-      return SizedBox(
-        width: _bannerAd!.size.width.toDouble(),
-        height: _bannerAd!.size.height.toDouble(),
-        child: AdWidget(ad: _bannerAd!),
-      );
-    } else {
-      return const SizedBox.shrink();
-    }
-  }
-
-  @override
-  void dispose() {
-    _bannerAd?.dispose();
-    super.dispose();
   }
 }
